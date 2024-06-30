6390db231d042a9dd03234f37ddbba39fb1a61c2
Aryan Singh <Aryannaruka7@gmail.com>
2024-06-30 17:15:19 +0530

    added the loader and the category section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
+import React from 'react';
 import Navbar from './component/Navbar';
-
-import Newz from './component/Newz';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Newz from './component/Newz'; // Adjust the path as per your project structure
 
 function App() {
   return (
     <>
+      <div>
+        <Router>
+          <Navbar />
+          <Newz key="sport" pagesize={8} country="in" category="sport" />
+          {/* <Switch>
+            <Route exact path="/sport">
+              <Newz key="sport" pagesize={5} country="in" category="sport" />
+            </Route>
+            <Route exact path="/general">
+              <Newz key="general" pagesize={5} country="in" category="general" />
+            </Route>
+            <Route exact path="/business">
+              <Newz key="business" pagesize={5} country="in" category="business" />
+            </Route>
+            <Route exact path="/health">
+              <Newz key="health" pagesize={5} country="in" category="health" />
+            </Route>
+            <Route exact path="/science">
+              <Newz key="science" pagesize={5} country="in" category="science" />
+            </Route>
+            <Route exact path="/technology">
+              <Newz key="technology" pagesize={5} country="in" category="technology" />
+            </Route>
+            <Route exact path="/entertainment">
+              <Newz key="entertainment" pagesize={5} country="in" category="entertainment" />
+            </Route>
+          </Switch> */}
 
-   <Navbar/>
-   <Newz/>
 
-  </>
+        </Router>
+      </div>
+    </>
   );
 }
 
--- src/component/Newz.js
+++ src/component/Newz.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 import NewzItems from "./NewzItems";
+import Loader from "./loader";
+import PropTypes from 'prop-types'
+
 
 export class Newz extends Component {
-  constructor(props) {
-    super(props);
+
+  static defaultProps ={
+    country : 'in',
+    pagesize : '5',
+    category : 'general'
+  }
+  static PropType ={
+    country : PropTypes.string,
+    pagesize : PropTypes.number,
+    category : PropTypes.string
+
+  }
+  constructor() {
+    super();
     this.state = {
       articles: [],
       page: 1,
@@ -15,22 +30,25 @@ export class Newz extends Component {
  
 
   fetchArticles = async (page = 1) => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c74edaad4e384eef919f7f58a89ef26d&page=${page}&pageSize=${this.props.pagesize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f1330346e890489f9793904ea0730765&page=${page}&pageSize=${this.props.pagesize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parseData = await data.json();
     this.setState({
       articles: parseData.articles,
       totalArticles: parseData.totalResults,
       page: page,
+      loading: false,
     });
   };
 
   componentDidMount = async () => {
+   
     await this.fetchArticles();
   };
 
   handleNextPage = async () => {
-    if (this.state.page + 1 <= Math.ceil(this.state.totalArticles / 20)) {
+    if (this.state.page + 1 <= Math.ceil(this.state.totalArticles / `${this.props.pagesize}`)) {
       await this.fetchArticles(this.state.page + 1);
     }
   };
@@ -42,15 +60,18 @@ export class Newz extends Component {
   };
 
   noMoreArticles = () => {
-    return this.state.page >= Math.ceil(this.state.totalArticles / 20);
+    return this.state.page >= Math.ceil(this.state.totalArticles /  `${this.props.pagesize}`);
   };
 
   render() {
     return (
+    
       <div className="container my-3">
         <h1 className="my-3 text-center">Top Headlines</h1>
+        <hr />
+    
         <div className="d-flex justify-content-between">
-          <button
+        <button
             disabled={this.state.page <= 1}
             type="button"
             onClick={this.handlePrevPage}
@@ -67,8 +88,13 @@ export class Newz extends Component {
             Next
           </button>
         </div>
+        {this.state.loading && (
+      <div className="d-flex justify-content-center my-3">
+        <Loader />
+      </div>
+    )}
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 my-1">
-          {this.state.articles.map((element) => (
+          {!this.state.loading && this.state.articles.map((element) => (
             <div className="col mb-4" key={element.url}>
               <NewzItems
                 title={element.title ? element.title.slice(0, 50) : ""}
@@ -81,7 +107,8 @@ export class Newz extends Component {
             </div>
           ))}
         </div>
-        <div className="d-flex justify-content-between">
+        
+        <div className=" d-flex justify-content-between" >
           <button
             disabled={this.state.page <= 1}
             type="button"
